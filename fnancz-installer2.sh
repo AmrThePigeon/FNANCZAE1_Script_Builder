8092521ffb0a1fa86ae9cf9a943e900815ae8fcf
AmrThePigeon <136938272+AmrThePigeon@users.noreply.github.com>
2026-04-21 22:38:41 +0200

    Remove directory existence check in installer script
    Removed directory existence check before creating game directory.

--- fnancz-installer2.sh
+++ fnancz-installer2.sh
@@ -2,10 +2,6 @@ echo -e "\e[31mP.S: you must give full path of the zip not a shortcut\e[0m"
 read -r -p "Where is the path that you have the zip of the game downloaded? Defaulted=Downloads:" path
 cd "$HOME/Downloads"
 cd "$path" # kinda stupid way that handles the operation but eh works for now
-if [ -d "Five Nights at NCZ AE" ]; then
-  echo "Error: Directory exists. Aborting."
-  exit 1
-fi
 gamezip=$(ls -v -r 'Five Nights at NCZ AE v'* | head -n 1)
 gamename="Five Nights at NCZ AE"
 mkdir "$gamename"
